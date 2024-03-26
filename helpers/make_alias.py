c46f0f1cff8b32ef5202ded1970119c664c779cd
Jonathan Cubides <jonathan.cubides@uib.no>
2024-03-26 08:18:14 +0100

    Add fuzzywuzzy library to create an alias index

--- helpers/make_alias.py
+++ helpers/make_alias.py
@@ -9,49 +9,98 @@ TODO:
 """
 
 from mkdocs.config import config_options
-from mkdocs.structure.files import File
-from mkdocs.structure.files import Files
+from mkdocs.config.defaults import MkDocsConfig
+from mkdocs.structure.files import File, Files
 from mkdocs.utils import meta, get_markdown_title, get_relative_url
 from pathlib import Path
 from typing import Any, Callable, List, Optional, Tuple
 from typing import Match
 import logging
-import os
 import re
-
 
 log: logging.Logger = logging.getLogger('mkdocs')
 
 # The Regular Expression used to find alias tags
 ALIAS_TAG_REGEX = r"([\\])?\[\[([^|\]]+)\|?([^\]]+)?\]\]"
-aliases: Any  = {}
-current_page:Any = None
-num_alias_issues: int = 0
-num_uses_of_aliases: int = 0
 
-def on_post_build(**_):
-    """Executed after the build has completed. Clears the aliases from
-    memory and displays stats if the verbose option is configured.
+from fuzzywuzzy import fuzz # type: ignore
+
+class FuzzyMap(dict):
+    ratio = 80
+
+    def closest_key(self, key):
+        """Returns the closest key matched by the given ratio"""
+        # print("len(self): ", len(self))
+        # print("self.keys(): ", self.keys())
+        # print("key: ", key)
+        # print("--------------------")
+        if len(self):
+
+            # Calculate matching coefficient of each key via fuzz.ratio
+            coefficients = {k: fuzz.UWRatio(k, key) for k in self.keys()}
+            matching = max(coefficients, key=lambda k: coefficients[k])
+            # print("coefficients: ", coefficients)
+            # print("matching: ", matching)
+            if coefficients[matching] >= self.ratio:
+                return matching
+        return key
+
+    def get(self, key, default=None):
+        return self[key] or default
+
+    def __missing__(self, key):
+        return super().get(self.closest_key(key))
+
+    def __setitem__(self, key, value):
+        super().__setitem__(self.closest_key(key), value)
+
+
+
+def on_pre_build(config: MkDocsConfig):
+    """Executed before the build begins. Initializes the aliases dictionary
+    and the alias issue and alias use counters.
     """
-    log.info("Defined %s alias(es).", len(aliases))
+    Path('docs/indexes').mkdir(parents=True, exist_ok=True)
+    # create the alias file if it doesn't exist
+    with open('docs/indexes/aliases.md', 'w') as f:
+        f.write("# Aliases\n\n")
+
+    config['aliases'] = FuzzyMap()
+    config['num_alias_issues'] = 0
+    config['num_uses_of_aliases'] = 0
+
+def on_post_build(config : MkDocsConfig):
+    aliases_dict = config.get('aliases', {})
+    num_alias_issues = config.get('num_alias_issues', 0)
+    num_uses_of_aliases = config.get('num_uses_of_aliases', 0)
+    log.info("Defined %s alias(es).", len(aliases_dict))
     log.info("Found %s alias issue(s).", num_alias_issues)
     log.info("Used %s alias(es).", num_uses_of_aliases)
-    aliases.clear()
 
-def on_page_markdown(markdown: str, *, page, **_):
+    with open('docs/indexes/aliases.md', 'w') as f:
+        f.write(f"<h1>Aliases <small>({len(aliases_dict)})</small></h1>\n\n")
+        current_letter = ''
+        for alias in sorted(aliases_dict.keys()):
+            if alias[0].upper() != current_letter:
+                current_letter = alias[0].upper()
+                f.write(f"## {current_letter}\n\n")
+            f.write(f"- [{alias}]({aliases_dict[alias]['url']})\n")
+    aliases_dict.clear()
+
+def on_page_markdown(markdown: str, *, page, config: MkDocsConfig, **_) -> str:
     """Replaces any alias tags on the page with markdown links."""
     current_page = page
     return re.sub(
         ALIAS_TAG_REGEX,
         lambda match: _replace_tag(
             match,
-            aliases,
-            current_page.file
+            current_page.file,
+            config
         ),
         markdown
     )
 
-def on_files(files, **_) -> None:
+def on_files(files, config: MkDocsConfig) -> None:
     """When MkDocs loads its files, extract aliases from any Markdown files
     that were found.
     """
@@ -61,10 +110,10 @@ def on_files(files, **_) -> None:
             alias_names : Optional[List[str]] = _get_alias_names(meta_data)
             if alias_names is None or len(alias_names) < 1:
                 _title:Optional[str] = _get_page_title(source, meta_data)
-                if _title and _title not in aliases:
+                if _title and _title not in config['aliases']:
                     _title = _title.strip()
                     alias_names = [_title]
-                    meta_data['alias'] = { 'name': _title }
+                    meta_data['alias'] = { 'name': _title , 'text': _title }
                 else:
                     continue
 
@@ -73,7 +122,7 @@ def on_files(files, **_) -> None:
                     '%s defines %d aliases:', file.url, len(alias_names)
                 )
             for alias in alias_names:
-                existing = aliases.get(alias)
+                existing = config['aliases'].get(alias)
                 if existing is not None:
                     log.warning(
                         "%s: alias %s already defined in %s, skipping.",
@@ -94,12 +143,12 @@ def on_files(files, **_) -> None:
                     ),
                     'url': file.src_uri,
                 }
-                log.info(
-                    "Alias %s to %s",
+                log.debug(
+                    "Alias '%s' to '%s'",
                     alias,
                     new_alias['url']
                 )
-                aliases[alias] = new_alias
+                config['aliases'][alias] = new_alias
 
 
 # Helper functions
@@ -130,11 +179,9 @@ def _get_alias_names(meta_data: dict):
 
 def _replace_tag(
     match: Match,
-    aliases: dict,
-    page_file: File
+    page_file: File,
+    config: MkDocsConfig
 ):
-    global num_alias_issues
-    global num_uses_of_aliases
     
     """Callback used in the sub function within on_page_markdown."""
     if match.group(1) is not None:
@@ -143,10 +190,18 @@ def _replace_tag(
     # split the tag up in case there's an anchor in the link
     tag_bits = ['']
     if match.group(2) is not None:
-        tag_bits = str(match.group(2)).split('#')
-    alias = aliases.get(tag_bits[0])
+        tag_bits = str(match.group(2)).strip().split('#')
+    if len(tag_bits) < 1:
+        log.warning(
+            "There is no anchor in the alias tag '%s' in '%s'",
+            match.group(2),
+            page_file.src_path)
+    
+    # assume the aliases are stored in a dictionary in the config
+    alias = config['aliases'].get(tag_bits[0])
+
     if alias is None:
-        num_alias_issues += 1
+        config['num_alias_issues'] += 1
         log.warning(
             "Alias '%s' not found in '%s'",
             match.group(2),
@@ -162,7 +217,7 @@ def _replace_tag(
     if len(tag_bits) > 1:
         url = f"{url}#{tag_bits[1]}"
 
-    num_uses_of_aliases += 1
+    config['num_uses_of_aliases'] += 1
     log.debug(
         "replaced alias '%s' with '%s' to '%s'",
         alias['alias'],
@@ -170,3 +225,25 @@ def _replace_tag(
         url
     )
     return f"[{text}]({url})"
+    
+
+# a = FuzzyMap()
+# a['test'] = '1'
+# a['Worker'] = '2'
+# a['Python Cookbook'] = '3'
+# a['Clean Code'] = '4'
+# a['The Pragmatic Programmer'] = '5'
+# a['Design Patterns'] = '6'
+# a['Code Complete'] = '7'
+# a['Refactoring'] = '8'
+# a['Effective Python'] = '9'
+# a['The Clean Coder'] = '10'
+# a['Cracking the Coding Interview'] = '11'
+
+# print("++++++++++++++")
+
+# if __name__ == '__main__':
+#     # read from the input
+#     while True:
+#         key = input("Enter a key: ")
+#         print(a[key])
