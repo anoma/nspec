caacba1f791a79e1927e07a1fb21fb9f13d4b487
Jonathan Cubides <jonathan.cubides@uib.no>
2024-09-27 23:43:46 +0200

    Add Juvix version in the footer of Juvix code blocks (#136)
    After merging this PR, the Juvix compiler version will be visible in the
    footer of Juvix code blocks. This is important because using a different
    Juvix version may cause problems when using the code. It has already
    happened to me.
    
    <img width="870" alt="image"
    src="https://github.com/user-attachments/assets/c46661ab-9e6b-4a39-afe7-9de5db4efd8b">
    
    <img width="869" alt="image"
    src="https://github.com/user-attachments/assets/7086a7ea-1dbe-4d49-84ff-f36b4bd79c85">

---
+++ hooks/juvix-codeblock-footer.py
@@ -0,0 +1,62 @@
+import logging
+import subprocess
+from pathlib import Path
+from typing import Optional
+
+from common.utils import fix_site_url
+from mkdocs.config.defaults import MkDocsConfig
+
+log: logging.Logger = logging.getLogger("mkdocs")
+
+ROOT_DIR = Path(__file__).parent.parent.absolute()
+DOCS_DIR = ROOT_DIR / "docs"
+ASSET_PATH = Path("assets") / "css" / "juvix-codeblock-footer.css"
+CSS_PATH = DOCS_DIR / ASSET_PATH
+
+
+def get_juvix_version() -> Optional[str]:
+    try:
+        result = subprocess.run(
+            ["juvix", "--numeric-version"],
+            stdout=subprocess.PIPE,
+            check=True,
+            text=True,
+        )
+        return result.stdout.strip()
+    except subprocess.CalledProcessError as e:
+        log.error("Failed to get Juvix version: %s", e)
+        return None
+
+
+def generate_css_file() -> Optional[Path]:
+    version = get_juvix_version()
+    if version is None:
+        log.error("Cannot generate CSS file without Juvix version.")
+        return None
+
+    css_content = f"""
+code.juvix::after {{
+    font-family: var(--md-code-font-family);
+    content: "Juvix v{version}";
+    font-size: 10px;
+    color: var(--md-juvix-codeblock-footer);
+    float: right;
+}}
+"""
+
+    CSS_PATH.parent.mkdir(parents=True, exist_ok=True)
+    with CSS_PATH.open("w") as f:
+        f.write(css_content)
+
+    log.info("CSS file generated at %s.", CSS_PATH)
+    return CSS_PATH
+
+
+def on_pre_build(config: MkDocsConfig) -> None:
+    generate_css_file()
+
+
+def on_config(config: MkDocsConfig) -> MkDocsConfig:
+    config = fix_site_url(config)
+    config["extra_css"].append(ASSET_PATH.as_posix())
+    return config
