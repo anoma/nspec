f844fada4f66aa1bcae8ca1ae60bf1b2d026ac39
Jonathan Cubides <jonathan.cubides@uib.no>
2024-12-22 12:13:30 -0500

    nspec-cli: add `version` command

--- hooks/cli.py
+++ hooks/cli.py
@@ -144,5 +144,45 @@ def engine(name):
     )
 
 
+@new.command()
+def version():
+    """Create a new version of the specification"""
+    version = questionary.text(
+        "What is the version of the new version? (e.g: `1.0.0`)"
+    ).ask()
+    click.echo(f"Creating version {Fore.GREEN}{version}{Fore.RESET}")
+    confirm = questionary.confirm(
+        "Are you sure you want to create this version? (y/n)"
+    ).ask()
+    if not confirm:
+        click.echo("Aborting")
+        return
+
+    # Update the version in the mkdocs.yml file
+    mkdocs_path = ROOT_PATH / "mkdocs.yml"
+    mkdocs_content = mkdocs_path.read_text()
+    mkdocs_content = re.sub(r"v[0-9]+\.[0-9]+\.[0-9]+", f"v{version}", mkdocs_content)
+    mkdocs_path.write_text(mkdocs_content)
+
+    # update the version in pyproject.toml
+    pyproject_path = ROOT_PATH / "pyproject.toml"
+    pyproject_content = pyproject_path.read_text()
+    pyproject_content = re.sub(r"version = \"[0-9]+\.[0-9]+\.[0-9]+\"", f"version = \"{version}\"", pyproject_content)
+    pyproject_path.write_text(pyproject_content)
+
+    # update the version in docs/Package.juvix
+    package_path = DOCS_PATH / "Package.juvix"
+    package_content = package_path.read_text()
+    #  mkVersion X Y Z;
+    version_parts = version.split(".")
+    package_content = re.sub(
+        r"mkVersion [0-9]+ [0-9]+ [0-9]+;",
+        f"mkVersion {version_parts[0]} {version_parts[1]} {version_parts[2]};",
+        package_content,
+    )
+    package_path.write_text(package_content)
+    
+
+
 if __name__ == "__main__":
     cli()
